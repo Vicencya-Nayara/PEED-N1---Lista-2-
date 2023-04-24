972acbecfbd5ab3ad462a266cb6703c4cb2c0aab
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-04-24 16:32:44 -0300

    Update Q7.py

--- Q7.py
+++ Q7.py
@@ -1,12 +1,11 @@
 #Peça para o usuário digitar 5 números e crie uma tupla com esses números. 
 #Em seguida, retorne o primeiro elemento da tupla.
 
-#Iniciando com a tupla vazia
-tupla = ()
+numeros = []
 
 for i in range(5):
     numero = int(input('Digite um número: '))
-    tupla.append(numero)
+    numeros.append(numero)
 
-tupla_numeros = tuple(numero)
+tupla_numeros = tuple(numeros)
 print('O primeiro elemento da tupla é:', tupla_numeros[0])
